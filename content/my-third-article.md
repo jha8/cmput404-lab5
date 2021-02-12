0a5934364c79f24360b634850fdccbc646849e19
jha8 <42952573+jha8@users.noreply.github.com>
2021-02-12 12:38:11 -0700

    add files

---
+++ content/my-third-article.md
@@ -0,0 +1,5 @@
+Title: My Third Article
+Date: 2021-02-12 11:00
+Category: Blog
+
+Testing third article.
--- publishconf.py
+++ publishconf.py
@@ -10,11 +10,13 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://jha8.github.io/cmput404-lab5/'
+OUTPUT_PATH = "docs/"
+THEME = 'templates/mytheme'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
